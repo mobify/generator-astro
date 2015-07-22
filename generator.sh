bcc89f571250c97daf302f78cf81a84738d4ae52
Shawn Jansepar <shawn@mobify.com>
2015-07-22 16:01:21 -0700

    Ask users to confirm, and also ask for bundle identifier

--- generator.sh
+++ generator.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
 echo "What do you want your project to be called?"
-
 read project_name
+
+# ensure the project name has no special characters
+project_name=$(echo $project_name | tr -dc '[:alnum:]\n\r' | tr '[:upper:]' '[:lower:]')
+
+# ask them to confirm the project name
+read -p "The project name is $project_name. Would you like to proceed? (y/n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
+# ask them for the app scheme
+echo "What app scheme do you want for deep linking into your iOS app?"
+read app_scheme
+
+# ask for the bundle identifier 
+echo "What Bundle Identifier would you like to use? (ex: com.yourcompany.yourapp)"
+read bundle_identifier
 
 mkdir $project_name
 cd $project_name
@@ -22,7 +40,10 @@ while true; do
     fi
 done
 
-# replace scaffold with $project_name inside of files
+# replace "com.mobify.astro" with $bundle_identifier inside of files
+egrep -lR "com\.mobify\.astro" . | tr '\n' '\0' | xargs -0 -n1 sed -i '' "s/com\.mobify\.astro/$bundle_identifier/g"  
+
+# replace "scaffold" with $project_name inside of files
 egrep -lR "scaffold" . | tr '\n' '\0' | xargs -0 -n1 sed -i '' "s/scaffold/$project_name/g"  
 
 # update submodule (this is done after replacing to
