25aa042a59a0e39fccbe79673fda53f85c50c34f
Shawn Jansepar <shawn@mobify.com>
2015-07-30 13:27:39 -0700

    Ask for host name for Android deep linking

--- generator.sh
+++ generator.sh
@@ -20,6 +20,10 @@ fi
 echo "What app scheme do you want for deep linking into your iOS app? (ex: facebook://, but don't include the ://)"
 read app_scheme
 
+# ask for the host name for Android deep linking
+echo "What host would you like to override for deep linking into your Android app? (ex: www.example.com)"
+read hostname
+
 # ask for the bundle identifier 
 echo "What Bundle Identifier would you like to use? (ex: com.yourcompany.yourapp)"
 read bundle_identifier
@@ -28,7 +32,7 @@ mkdir $project_name
 cd $project_name
 
 git init
-git pull git@github.com:mobify/astro-scaffold.git --depth 1
+git pull git@github.com:mobify/astro-scaffold.git@deeplinking-refactor --depth 1
 
 # replace scaffold in the names of different files and folders with $project_name
 while true; do
@@ -44,6 +48,9 @@ done
 
 # replace "com.mobify.astro" with $bundle_identifier inside of files
 egrep -lR "com\.mobify\.astro" . | tr '\n' '\0' | xargs -0 -n1 sed -i '' "s/com\.mobify\.astro/$bundle_identifier/g" 
+
+# replace "www.mobify.com" with $hostname inside of the AndroidManifest
+egrep -lR "android:host=\"www.mobify.com\"" . | tr '\n' '\0' | xargs -0 -n1 sed -i '' "s/android:host=\"www.mobify.com\"/android:host=\"$hostname\"/g"
 
 # replace "scaffold" with $project_name inside of files
 egrep -lR "scaffold" . | tr '\n' '\0' | xargs -0 -n1 sed -i '' "s/scaffold/$project_name/g" 2>/dev/null 
