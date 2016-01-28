22f02e6ca6ac01e9cf67170255aa82a41ac9c369
Jeremy Wiebe <jeremy@mobify.com>
2016-01-28 14:53:13 -0800

    Generator finally works with release zip file!!

--- generator.sh
+++ generator.sh
@@ -62,21 +62,17 @@ mkdir "$project_dir"
 cd "$project_dir" || exit
 git init
 
-set -x
 # Download the scaffold and copy it into the project directory
 WORKING_DIR=$(mktemp -d /tmp/astro-scaffold.XXXXX)
 echo "Working directory: $WORKING_DIR"
 trap 'rm -rf "$WORKING_DIR"' EXIT
+
 curl --progress-bar -L "$SCAFFOLD_URL" -o "$WORKING_DIR/astro-scaffold-$SCAFFOLD_VERSION.zip"
-ls "$WORKING_DIR"
 cd $WORKING_DIR || exit
-unzip "$WORKING_DIR/astro-scaffold-$SCAFFOLD_VERSION.zip"
-ls "$WORKING_DIR"
-cp -R "$WORKING_DIR/astro-scaffold-$SCAFFOLD_VERSION" "$project_dir"
-ls "$project_dir"
+unzip -q "$WORKING_DIR/astro-scaffold-$SCAFFOLD_VERSION.zip"
+cp -R "$WORKING_DIR/astro-scaffold-$SCAFFOLD_VERSION/" "$project_dir"
 
 cd "$project_dir"
-set +x
 
 # Set up CI support
 if [[ $ios_ci_support -ne 1 && $android_ci_support -ne 1 ]]; then
