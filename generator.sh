871a0180dcfd71c13a82bf19a63674f7cc9227fa
Tony Dinh <tdinh@mobify.me>
2016-03-22 12:04:54 -0700

    rename variable for clarify on running against scaffold branch

--- generator.sh
+++ generator.sh
@@ -4,7 +4,7 @@ set -o pipefail
 MYDIR=$(pwd)
 ROOT=$MYDIR # In some scripts ROOT != MYDIR
 
-SCAFFOLD_VERSION=0.8.0
+SCAFFOLD_VERSION_OR_BRANCH=0.8.0
 SCAFFOLD_URL="https://github.com/mobify/astro-scaffold/archive/$SCAFFOLD_VERSION.zip"
 
 echo '                                '
